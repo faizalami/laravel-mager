5ff956c95b96ca7a0ef2dbae63c9a45ed7c3010d
faizalami <f.a.faizal.amiruddin@gmail.com>
2019-01-21 18:58:46 +0700

    generator-system: change default PHP filesystem functions to Laravel File facade

--- publishes/views/layouts/main.blade.php
+++ publishes/views/layouts/main.blade.php
@@ -15,7 +15,7 @@
 
     <header class="main-header">
         <!-- Logo -->
-        <a href="{{ route('mager.dashboard') }}" class="logo">
+        <a href="{{ url('/') }}" class="logo">
             <!-- mini logo for sidebar mini 50x50 pixels -->
             <span class="logo-mini"><b>L</b>M</span>
             <!-- logo for regular state and mobile devices -->
